0b837934ca14dbe03f7776322c18c537386423cd
Anshul <anshul3071@gmail.com>
2024-06-21 00:11:55 +0530

    Updated counter

--- Learning_React/03CountUpdater/src/components/Layout.jsx
+++ Learning_React/03CountUpdater/src/components/Layout.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
+import Display from './Display'
+import { useDispatch } from 'react-redux'
 
 const Layout = () => {
-    return (
+    const dispatch = useDispatch();
 
-        <div className='max-w-[400px] border-[1px] h-[250px] rounded-lg shadow-md mx-auto'>
+    const handleIncrement = () => {
+        dispatch({type: "INCREMENT"});
+    }
+
+    const handleDecrement = () => {
+        dispatch({type : "DECREMENT"});
+    }
+
+    return (
+        <div className='max-w-[400px] border-[1px] h-[250px] rounded-lg shadow-lg  mx-auto'>
             <div className='text-center'>
                 <h1 className='font-bold text-xl mt-2'>Counter</h1>
-                <div className='mt-5'>Current counter value : </div>
+                <Display/>
             </div>
-            <div className='flex justify-center items-center '>
-                <button className='m-2 p-1 px-2 rounded-full bg-green-600'>Increase</button>
-                <button className='m-2 p-1 px-2 rounded-full bg-red-600'>Decrease</button>
+            <div className='flex justify-center items-center mt-4'>
+                <button onClick={handleIncrement} className='m-2 p-2 px-2 rounded-full bg-green-600'>Increase</button>
+                <button onClick={handleDecrement} className='m-2 p-2 px-2 rounded-full bg-red-600'>Decrease</button>
             </div>
 
+            <div className="flex justify-center mt-2">
+                <input type="text" placeholder='Enter Number' className='shadow-md max-w-[130px] p-3 border-none outline-none'/>
+                <button className='m-2 p-1 px-5 rounded-full bg-green-600'>+</button>
+                <button className='m-2 p-1 px-5 rounded-full bg-red-600'>-</button>
+            </div>
         </div>
     )
 }
